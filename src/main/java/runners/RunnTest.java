01984149b1423084c4ac5cd625e2f9e1448c4463
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2018-03-18 15:19:41 +0600

    initCommit

---
+++ src/main/java/runners/RunnTest.java
@@ -0,0 +1,19 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/features",
+        glue = "ru.savkk.test",
+        tags = "@all",
+        dryRun = false,
+        strict = false,
+        snippets = SnippetType.UNDERSCORE
+)
+
+public class RunTest {
+
+}
